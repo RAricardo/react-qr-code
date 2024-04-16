70af18f8374a4ffc1d4465112cb369bdc8aa01d2
RAricardo <ricardoazo@gmail.com>
2024-04-16 16:54:21 -0500

    fea: solution submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card />
     </div>
   );
 }
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div>
+        <img
+          className="qr"
+          src={process.env.PUBLIC_URL + "/img/image-qr-code.png"}
+        />
+      </div>
+      <div className="card-content">
+        <h2 className="card-title">
+          Improve your front-end skills by building projects
+        </h2>
+        <h3 className="card-desc">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </h3>
+      </div>
+    </div>
+  );
+};
 
 export default App;
